99382b7a00bc5cbdd5562217f702b49e35960404
Mats Faugli <mats.faugli@gmail.com>
2022-08-31 11:04:26 +0200

    LSP and formatting for lua, go and web stuff

---
+++ chadrc.lua
@@ -0,0 +1,59 @@
+local M = {}
+
+M.ui = {
+  theme = "vscode_dark",
+}
+
+M.plugins = {
+  user = {
+    ["neovim/nvim-lspconfig"] = {
+      config = function()
+        require "plugins.configs.lspconfig"
+        require "custom.plugins.lspconfig"
+      end,
+    },
+
+    ["jose-elias-alvarez/null-ls.nvim"] = {
+      after = "nvim-lspconfig",
+      config = function()
+        require "custom.plugins.null-ls"
+      end,
+    },
+  },
+
+  override = {
+    ["nvim-treesitter/nvim-treesitter"] = {
+      ensure_installed = {
+        "lua",
+        "html",
+        "css",
+        "go",
+      },
+    },
+
+    ["williamboman/mason.nvim"] = {
+      ensure_installed = {
+        -- lua stuff
+        "lua-language-server",
+        "stylua",
+
+        -- web dev
+        "css-lsp",
+        "html-lsp",
+        "emmet-ls",
+        "json-lsp",
+
+        -- go
+        "gopls",
+        "goimports",
+      },
+    },
+
+    -- Only load whichkey after all the gui
+    ["folke/which-key.nvim"] = {
+      disabled = true,
+    },
+  },
+}
+
+return M
