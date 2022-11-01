5811f546795ec4a9b33d6e9076a7d77c048a66dc
Mats Faugli <mats.faugli@dnb.no>
2022-11-01 10:00:26 +0100

    Use alt-keybindings for window navigation

--- mappings.lua
+++ mappings.lua
@@ -5,6 +5,38 @@ M.mappings = {
     ["<C-p>"] = { "<cmd> Telescope <CR>", "open Telescope" },
     ["<C-q>"] = { "<cmd> qa <CR>", "quit Neovim" },
     ["<leader>ts"] = { "<cmd> SymbolsOutline <CR>", "toggle symbol outline" },
+
+    -- switch between windows with colemak-dh
+    ["<A-n>"] = { "<C-w>h", "window left" },
+    ["<A-o>"] = { "<C-w>l", "window right" },
+    ["<A-e>"] = { "<C-w>j", "window down" },
+    ["<A-i>"] = { "<C-w>k", "window up" },
+
+    -- line numbers
+    ["<leader>n"] = { "<cmd> set nu! <CR>", "toggle line number" },
+    ["<leader>rn"] = { "<cmd> set rnu! <CR>", "toggle relative number" },
+  },
+}
+
+M.nvterm = {
+  t = {
+    -- toggle in terminal mode
+    ["<A-t>"] = {
+      function()
+        require("nvterm.terminal").toggle "float"
+      end,
+      "toggle floating term",
+    },
+  },
+
+  n = {
+    -- toggle in normal mode
+    ["<A-t>"] = {
+      function()
+        require("nvterm.terminal").toggle "float"
+      end,
+      "toggle floating term",
+    },
   },
 }
 
